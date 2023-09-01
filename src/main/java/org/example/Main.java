8457c9c7c9c9c8dd6f01b747ac3e6740489a4b99
Libi Gurevich <l.zholudeva@gmail.com>
2023-09-01 18:27:23 +0300

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
         System.out.println("main changes");
 
+        System.out.println("2");
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
